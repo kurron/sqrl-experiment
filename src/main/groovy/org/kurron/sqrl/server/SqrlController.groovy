a94dc1889a60ee56e4fa665ba7e10e826e459337
Ron Kurr <kurr@kurron.org>
2013-10-04 21:21:05 -0400

    Got a skeleton controller up and running.

---
+++ src/main/groovy/org/kurron/sqrl/server/SqrlController.groovy
@@ -0,0 +1,20 @@
+package org.kurron.sqrl.server
+
+import groovy.util.logging.Slf4j
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+
+/**
+ * Spring MVC controller that understands the SQRL authentication protocol.
+ */
+@Slf4j
+@Controller
+class SqrlController {
+    @RequestMapping( value = '/sqrl', method = RequestMethod.GET )
+    @ResponseBody
+    String bob() {
+        'bob'
+    }
+}
