7daa468b09afd615179e4ffa8f73ca7672843523
Ron Kurr <kurr@kurron.org>
2013-10-04 23:32:56 -0400

    Did some minor refactorings.

--- src/main/groovy/org/kurron/sqrl/client/SqrlClient.groovy
+++ src/main/groovy/org/kurron/sqrl/client/SqrlClient.groovy
@@ -1,19 +1,22 @@
 package org.kurron.sqrl.client
 
 import groovy.util.logging.Slf4j
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.client.ClientHttpResponse
 import org.springframework.web.client.ResponseErrorHandler
-import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
 
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 import java.security.KeyPair
 import java.security.KeyPairGenerator
-import java.security.MessageDigest
+import java.security.Provider
 import java.security.SecureRandom
+import java.security.Security
 import java.security.Signature
 
 /**
@@ -28,32 +31,58 @@ class SqrlClient  implements ResponseErrorHandler {
         RestTemplate template = new RestTemplate()
         template.errorHandler = new SqrlClient()
         ResponseEntity<String> entity = template.getForEntity(new URI('http://localhost:8080/sqrl'), String)
+
         if ( entity.statusCode == HttpStatus.UNAUTHORIZED ) {
             String challenge = entity.headers.getFirst('X-SQRL-CHALLENGE')
             log.info( 'Challenge {} received.', challenge )
 
-            // create an HMAC of the domain
-            Mac hmac = Mac.getInstance('HmacSHA256')
-            byte[] secretBytes = new byte[256]
-            generator.nextBytes( secretBytes )
-            hmac.init( new SecretKeySpec( secretBytes, 'AES' ) )
-            byte[] digest = hmac.doFinal( 'localhost'.bytes )
-
-            // generate a new key pair using the RSA algorithm
-            KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance('RSA')
-            KeyPair pair = keyGenerator.generateKeyPair()
-
-            // store the private key and index it by the HMAC of the domain
-            map[digest] = pair.private
-
-            // digest the challenge
-            Signature signer = Signature.getInstance( 'SHA256withRSA' )
-            signer.initSign( pair.private, generator )
-            signer.update( challenge.bytes )
-            byte[] signature = signer.sign()
+            byte[] digest = generateDomainDigest('localhost')
+            KeyPair pair = generateKeyPair()
+            storePrivateKey(pair, map, digest)
+            byte[] signature = signChallenge(pair, challenge)
+            String asString = new String(signature)
+            HttpHeaders headers = new HttpHeaders()
+            headers.add( 'Authorization', 'signature:signature' )  // must be in a base64 encoded string
+            ResponseEntity<String> resource = template.exchange( new URI('http://localhost:8080/sqrl'), HttpMethod.POST, new HttpEntity<byte[]>( pair.public.encoded, headers ), String )
 
             println()
         }
+    }
+
+    private static void storePrivateKey(KeyPair pair, Map map, byte[] digest) {
+// store the private key and index it by the HMAC of the domain
+        map[digest] = pair.private
+    }
+
+    private static byte[] signChallenge(KeyPair pair, String challenge) {
+// digest the challenge
+        Signature signer = Signature.getInstance('SHA256withRSA')
+        signer.initSign(pair.private, generator)
+        signer.update(challenge.bytes)
+        byte[] signature = signer.sign()
+        signature
+    }
+
+    private static KeyPair generateKeyPair() {
+// generate a new key pair using the RSA algorithm
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance('RSA')
+        KeyPair pair = keyGenerator.generateKeyPair()
+        pair
+    }
+
+    private static byte[] generateDomainDigest(String domain) {
+// create an HMAC of the domain
+        Mac hmac = Mac.getInstance('HmacSHA256')
+        hmac.init(generateSecretHmacKey())
+        byte[] digest = hmac.doFinal(domain.bytes)
+        digest
+    }
+
+    private static SecretKeySpec generateSecretHmacKey() {
+        byte[] secretBytes = new byte[256]
+        generator.nextBytes(secretBytes)
+        SecretKeySpec secretKey = new SecretKeySpec(secretBytes, 'AES')
+        secretKey
     }
 
     @Override
