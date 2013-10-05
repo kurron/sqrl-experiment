d9d2e40e7bdd741ee3ecaaec4385719b59d8a92e
Ron Kurr <kurr@kurron.org>
2013-10-04 21:35:58 -0400

    Can return headers and a body.

--- src/main/groovy/org/kurron/sqrl/server/SqrlController.groovy
+++ src/main/groovy/org/kurron/sqrl/server/SqrlController.groovy
@@ -1,11 +1,12 @@
 package org.kurron.sqrl.server
 
 import groovy.util.logging.Slf4j
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
 
 /**
  * Spring MVC controller that understands the SQRL authentication protocol.
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody
 @Controller
 class SqrlController {
     @RequestMapping( value = '/sqrl', method = RequestMethod.GET )
-    @ResponseBody
-    String bob( @RequestHeader( value = 'Authorization', required = false ) String authenticationString ) {
-        log.info( 'Authorization header = {}', authenticationString)
-        'bob'
+    HttpEntity<String>  bob( @RequestHeader( value = 'Authorization', required = false ) String authenticationString ) {
+        log.info( 'Authorization header = {}', authenticationString )
+        HttpHeaders headers = new HttpHeaders()
+        headers.add( 'X-Custom', 'Logan' )
+        HttpEntity<String> response = new HttpEntity<>( 'body', headers )
+        response
     }
 }
